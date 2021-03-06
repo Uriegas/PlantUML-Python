00369ad6ba7effc9795c8bde06241195311fb196
Uriegas <1930526@upv.edu.mx>
2021-03-06 08:07:25 -0600

    Ideas

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import os
 # So
+class PlantumConverter:
+    def __init__(self):
+        self.t
+
+#Primero obtener los archivos
+#Despues buscar las clases
+#Las funciones de las clases
+#Las relaciones de las clases
+#Información en: https://plantuml.com/class-diagram
+
+#El programa genera un txt
+#Ese txt se manda a plantuml
+#Finalmente se muestra el diagrama
+
 
 #
 os.system("java -jar plantuml.jar DiagramaClases.txt") # Termine sin error
